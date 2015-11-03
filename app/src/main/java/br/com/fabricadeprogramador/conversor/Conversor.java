adec07605ba0e0409bf633c39a327f60228bfd99
thiago <thiagocouve@gmail.com>
2015-11-03 20:57:53 -0200

    first commit

---
+++ app/src/main/java/br/com/fabricadeprogramador/conversor/Conversor.java
@@ -0,0 +1,13 @@
+package br.com.fabricadeprogramador.conversor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Conversor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversor);
+    }
+}
